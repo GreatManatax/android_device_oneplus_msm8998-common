08db8e46adffac089bb6508b26b767c3202e3f52
Adhitya Mohan <me@adhityamohan.in>
2020-03-22 13:34:49 +0530

    Uprev radio and built some utils for qcril

--- common.mk
+++ common.mk
@@ -271,7 +271,7 @@ PRODUCT_PACKAGES += \
     org.ifaa.android.manager
 
 PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
+    org.ifaa.android.manager \
 
 # IPv6
 PRODUCT_PACKAGES += \
@@ -407,7 +407,11 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     ims-ext-common_system \
-    telephony-ext
+    telephony-ext \
+		qti-telephony-hidl-wrapper \
+		qti_telephony_hidl_wrapper.xml \
+		qti-telephony-utils \
+		qti_telephony_utils.xml \
 
 PRODUCT_BOOT_JARS += \
     ims-ext-common_system \
